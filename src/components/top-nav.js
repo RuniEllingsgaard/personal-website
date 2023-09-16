8d4998fb324d5874080c0484a3dcaf9ad6606836
Runi Ellingsgaard <runi@usestudio.dk>
2023-09-16 23:55:46 +0200

    Getting stuff separated as components and getting the filter nav back alive again

---
+++ src/components/top-nav.js
@@ -0,0 +1,26 @@
+import * as React from "react"
+import "../styles/styles.css"
+
+function TopNav(){
+  return(
+    <div className="header">
+        <div className="site-info-container">
+          <div className="name-version-container">
+            <p className="site-name">Runi Baldvinsson</p>
+            <p className="site-version">ver. 3.1</p>
+            <p className="site-position">Partner & Head of Digital Experience @ USE</p>
+          </div>
+          <h1 className="site-title">A senior Digital Product Designer, who codes</h1>
+        </div>
+        {/* <div className="nav-container">
+          <span>About me</span>
+          <div className="contact-container">
+            <button className="site-contact-button">Contact</button>
+          </div>
+        </div> */}
+      </div>
+
+  )
+}
+export default TopNav;
+
